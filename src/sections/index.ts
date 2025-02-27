21951fb2228052dea2dadc8134964f0f2f0a5911
ProtoSg <diegoalbertosalazar177@gmail.com>
2025-02-27 17:04:12 -0500

    feat: add and update sections

---
+++ src/sections/index.ts
@@ -0,0 +1,6 @@
+export { default as Hero } from "./Hero.astro";
+export { default as Skills } from "./Skills.astro";
+export { default as Projects } from "./Projects.astro";
+export { default as Header } from "./Header.astro";
+export { default as AboutMe } from "./AboutMe.astro";
+export { default as Footer } from "./Footer.astro";
