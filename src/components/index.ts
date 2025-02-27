1fcaa020d8d95102c11cc89e9adfcd75c36e7f8d
ProtoSg <diegoalbertosalazar177@gmail.com>
2025-02-27 17:03:26 -0500

    feat: add and update components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Card } from "./Card.astro";
+export { default as PrimaryLinkButton } from "./PrimaryLinkButton.astro";
+export { default as Title } from "./Title.astro";
+export { default as Section } from "./Section.astro";
+export { default as ItemProject } from "./ItemProject.astro";
+export { default as SecundaryLinkButton } from "./SecundaryLinkButton.astro";
