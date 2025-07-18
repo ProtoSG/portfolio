5201f1a0be69748b86fc0cdf922c8bb32ff01154
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-07-18 15:35:45 -0500

    feat: add config for deploy in github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: "https://protosg.github.io",
+  output: "static",
+  base: "/portfolio/",
 });
